c17ead216d0d23b4d8a2531d927e747c26cccc3c
nik67 <nikhilsahni240@gmail.com>
2023-12-31 14:22:33 +0000

    about let var and const

--- 1_basic/test.js
+++ 1_basic/test.js
@@ -1,11 +1,27 @@
 
 
 const accountNumber = 12345;
-const accountNumber = "happy";
+
 accountCity = "jaipur";
 
 let emailId = "Nikhilsahni111@gmail.com";
 
 var phoneNumber = 1234567;
 
-console.log(accountNumber);
+// now we add more changes to the respective variable
+
+accountNumber =54321;
+
+accountCity = "patna";
+
+emailId = "dumb@gmail.com";
+
+var phoneNumber = 7654321;
+
+console.table([accountNumber, accountCity, emailId, phoneNumber]);
+
+/* make a note 
+*that don't use var for variable naming.
+*since the var failed in scope block and functional scope so for better we use let and const as for variable decalaration.
+
+*/
