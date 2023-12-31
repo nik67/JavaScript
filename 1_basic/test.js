3038252906b71c2fa2f6fb6bea4d3294ee7b5263
nik67 <nikhilsahni240@gmail.com>
2023-12-31 13:59:02 +0000

    container config done as well as first programm

---
+++ 1_basic/test.js
@@ -0,0 +1,11 @@
+
+
+const accountNumber = 12345;
+const accountNumber = "happy";
+accountCity = "jaipur";
+
+let emailId = "Nikhilsahni111@gmail.com";
+
+var phoneNumber = 1234567;
+
+console.log(accountNumber);
