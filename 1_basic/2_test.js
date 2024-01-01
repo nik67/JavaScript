bb16a02e2c96861abfdabecfa80b5ea87cacd18f
nik67 <nikhilsahni240@gmail.com>
2024-01-01 13:38:48 +0000

    datatypes

---
+++ 1_basic/2_test.js
@@ -0,0 +1,25 @@
+//Data type//
+
+"use strict"; // we use this use strict treat all js as newer version syntax like class,arrow function etc.//
+
+// alert( 3 + 3 ) // we are using nodejs not browser//
+
+console.log(3+3);
+
+console.log("zipp");
+
+
+// Number => 2 to pow 53
+// bigInt => 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => var decalred but no value defined.
+// eg. let varName;  no value define or null used.
+// symbol => used in figma and react for uniqueness.
+
+//object
+// below code null is behaving as object
+
+console.log(typeof undefined);
+console.log(typeof null);
